ecfc5d67d1a1a45e3366e10a455f383bca76905d
alieldin70 <103864967+alieldin70@users.noreply.github.com>
2024-05-25 00:09:48 +0300

    Delete TransactionsManagementSystem directory

--- TransactionsManagementSystem/src/main/java/com/realProject/salesManagement/Dao/clientJpaRpos.java
+++
@@ -1,9 +0,0 @@
-package com.realProject.salesManagement.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.realProject.salesManagement.Entity.Client;
-
-public interface clientJpaRpos extends JpaRepository<Client ,Integer> {
- 
-}
--- TransactionsManagementSystem/src/main/java/com/realProject/salesManagement/ExcpHand/IdNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.realProject.salesManagement.ExcpHand;
-//custom type of exception 
-public class IdNotFoundException extends RuntimeException {
-
-	public IdNotFoundException(String message) {
-		super(message);
-	}
-
-}
